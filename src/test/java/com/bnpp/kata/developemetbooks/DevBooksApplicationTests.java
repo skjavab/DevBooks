4025e9b3f4640f2bbe7b9ea6c708cbd43acedcde
skjavab <srshryls@gamil.com>
2022-04-01 18:13:42 +0530

    Initial Spring Boot project setup for Development Books

---
+++ src/test/java/com/bnpp/kata/developemetbooks/DevBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpp.kata.developemetbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
