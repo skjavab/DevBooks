5bb7b5eb8021d9f55deecd2eb2f3d581dae7db40
2022-DEV1-018 <2022.dev1.018@gmail.com>
2022-04-02 05:39:57 +0530

    Code refactoring add clean

--- src/main/java/com/bnpp/kata/developemetbooks/model/BookApiRequest.java
+++ src/main/java/com/bnpp/kata/developemetbooks/model/BookApiRequest.java
@@ -7,8 +7,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BookApiRequest {
+public class BookApiRequest implements Comparable<BookApiRequest> {
 
 	private Integer bookId;
 	private Integer quantity;
+	@Override
+	public int compareTo(BookApiRequest arg) {
+		if (getQuantity() == null || arg.getQuantity() == null) {
+			return 0;
+		}
+		return getQuantity().compareTo(arg.getQuantity());
+	}
 }
--- src/main/java/com/bnpp/kata/developemetbooks/service/CalculateBooksPriceService.java
+++ src/main/java/com/bnpp/kata/developemetbooks/service/CalculateBooksPriceService.java
@@ -1,38 +1,26 @@
 package com.bnpp.kata.developemetbooks.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import com.bnpp.kata.developemetbooks.constant.CalculateBooksConstant;
 import com.bnpp.kata.developemetbooks.model.BookApiRequest;
 import com.bnpp.kata.developemetbooks.model.DiscountEnum;
 
 public class CalculateBooksPriceService {
+	private int maxDiscountSet;
 
 	public double calculateBooksPrice(List<BookApiRequest> bookApiRequestList) {
-		double calculatedPrice = 0d;
-		if (bookApiRequestList.size() == 1) {
-			BookApiRequest bookApiRequest = bookApiRequestList.get(0);
-			calculatedPrice = CalculateBooksConstant.PRICE * bookApiRequest.getQuantity();
-		} else if (bookApiRequestList.size() == 2) {
-			double basePrice = bookApiRequestList.size() * CalculateBooksConstant.PRICE;
-			double discountedPriceForSet = basePrice - ((basePrice * 5) / CalculateBooksConstant.PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		} else if (bookApiRequestList.size() == 3) {
-			double basePrice = bookApiRequestList.size() * CalculateBooksConstant.PRICE;
-			double discountedPriceForSet = basePrice - ((basePrice * 10) / CalculateBooksConstant.PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		} else if (bookApiRequestList.size() == 4) {
-			double basePrice = bookApiRequestList.size() * CalculateBooksConstant.PRICE;
-			double discountedPriceForSet = basePrice - ((basePrice * 20) / CalculateBooksConstant.PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		} else if (bookApiRequestList.size() == 5) {
-			double basePrice = bookApiRequestList.size() * CalculateBooksConstant.PRICE;
-			double discountedPriceForSet = basePrice - ((basePrice * 25) / CalculateBooksConstant.PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		}
-		return calculatedPrice;
+	maxDiscountSet = bookApiRequestList.size();
+	Collections.sort(bookApiRequestList, Collections.reverseOrder());
+	List<Integer> inputBookIds = getBookIdList(bookApiRequestList);
+	List<List<Integer>> defaultCombinationSets = getDefaultBooksCombination(inputBookIds);
 
+	
+	double totalPriceNormalSets = getBooksPrice(defaultCombinationSets);
+	
+	return totalPriceNormalSets;
+	
 	}
 
 	public List<Integer> getBookIdList(List<BookApiRequest> bookInputList) {
@@ -102,5 +90,6 @@ public class CalculateBooksPriceService {
 		calculatedPrice += discountedPriceForSet;
 		return calculatedPrice;
 	}
+	
 
 }
